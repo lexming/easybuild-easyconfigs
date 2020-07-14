21b8024e1cdfb574e0d337045cf1547990e8fc5f
Alex Domingo <alex.domingo.toro@vub.be>
2020-07-14 23:05:18 +0200

    RELION: adapt v3.1.0 to Hydra

--- easybuild/easyconfigs/r/RELION/qsub_hydra.bash
+++ easybuild/easyconfigs/r/RELION/qsub_hydra.bash
@@ -2,6 +2,7 @@
 #PBS -N XXXnameXXX
 #PBS -l nodes=XXXmpinodesXXX:ppn=XXXthreadsXXX
 #PBS -l walltime=XXXextra1XXX:00:00
+#PBS -q XXXqueueXXX
 #PBS -e XXXerrfileXXX
 #PBS -o XXXoutfileXXX
 
--- easybuild/easyconfigs/r/RELION/qsub_hydra_gpu.bash
+++ easybuild/easyconfigs/r/RELION/qsub_hydra_gpu.bash
@@ -2,11 +2,15 @@
 #PBS -N XXXnameXXX
 #PBS -l nodes=XXXmpinodesXXX:ppn=XXXthreadsXXX:gpus=XXXextra2XXX
 #PBS -l walltime=XXXextra1XXX:00:00
+#PBS -q XXXqueueXXX
 #PBS -e XXXerrfileXXX
 #PBS -o XXXoutfileXXX
 
 cd $PBS_O_WORKDIR
 
-module load RELION/3.1.0-foss-2019b
+module load RELION/3.1.0-fosscuda-2019b
+
+export CUDA_MPS_PIPE_DIRECTORY=$TMPDIR/nvidia-mps
+nvidia-cuda-mps-control -d
 
 mpirun -n XXXmpinodesXXX XXXcommandXXX
